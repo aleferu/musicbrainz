4e44de8b5c253455a981d15f09eaaeedb230e995
aleferu <aleferu888@gmail.com>
2024-08-15 17:47:57 +0200

    feat: Changed it so that now each release is a node, pending of approval...

--- neo4j_import.py
+++ neo4j_import.py
@@ -10,11 +10,14 @@ from typing import LiteralString
 from neo4j import Driver, GraphDatabase, basic_auth
 
 
-def execute_and_print_query(driver: Driver, query: LiteralString):
+def execute_query(driver: Driver, query: LiteralString, print_records: bool = False):
     with driver.session() as session:
+        print(f"Querying '{query}'...")
         result = session.run(query)
-        for record in result:
-            print(record)
+        if print_records:
+            print("Printing records:")
+            for record in result:
+                print(record)
 
 
 def import_artists(driver: Driver):
@@ -23,20 +26,30 @@ def import_artists(driver: Driver):
     # If stuck: google how to use apoc for loading JSON files in Neo4j
     # It's really simple, moving a .jar file and a few settings
     # You also need to "cp artists.jsonl /var/lib/neo4j/import/artists.jsonl"
+    query = "CREATE INDEX IF NOT EXISTS FOR (a:Artist) ON (a.main_id);"
+    execute_query(driver, query)
+
     query = """
         CALL apoc.periodic.iterate(
             "CALL apoc.load.json('file:///artists.jsonl') YIELD value",
-            "MERGE (n:Artist {main_id: value.main_id, known_ids: value.known_ids, known_names: value.known_names})",
+            "MERGE (n:Artist {
+                main_id: value.main_id,
+                known_ids: value.known_ids,
+                known_names: value.known_names,
+                tags: [],
+                listeners: 0,
+                playcount: 0
+            })",
             {batchSize: 10000, parallel: true, concurrency: 8}
         );
     """
-    execute_and_print_query(driver, query)
-
-    query = "CREATE INDEX IF NOT EXISTS FOR (a:Artist) ON (a.main_id);"
-    execute_and_print_query(driver, query)
+    execute_query(driver, query, True)
 
 
 def import_releases(driver: Driver):
+    query = "CREATE INDEX IF NOT EXISTS FOR (r:Release) ON (r.id);"
+    execute_query(driver, query)
+
     query = """
         CALL apoc.periodic.iterate(
             "LOAD CSV WITH HEADERS FROM 'file:///releases_no_va_merged_id.csv' AS row RETURN row",
@@ -44,29 +57,24 @@ def import_releases(driver: Driver):
                 WITH
                     [row.a0_id, row.a1_id, row.a2_id, row.a3_id, row.a4_id] AS ids,
                     row
-                UNWIND ids AS id0
-                UNWIND ids AS id1
-                MATCH (a0:Artist {main_id: id0}), (a1:Artist {main_id: id1})
-                WHERE id0 < id1
+                UNWIND ids AS artist_id
                 WITH
-                    a0,
-                    a1,
+                    artist_id,
+                    row
+                MATCH (a:Artist {main_id: artist_id})
+                WITH
+                    a,
                     row,
                     date(row.date) AS coll_date
-                MERGE (a0)-[r:COLLABORATED_WITH]->(a1)
-                    ON CREATE SET
-                      r.names = [row.name],
-                      r.dates = [coll_date],
-                      r.ids = [row.id]
-                    ON MATCH SET
-                      r.names = r.names + [row.name],
-                      r.dates = r.dates + [coll_date],
-                      r.ids = r.ids + [row.id]
+                MERGE (r:Release {id: row.id})
+                ON CREATE SET r.name = row.name, r.date = coll_date, r.artist_count = row.artist_count
+                MERGE (a)-[:WORKED_IN]->(r)
+                MERGE (r)-[:WORKED_BY]->(a)
             ",
-            {batchSize: 50000, parallel: false, concurrency: 8}
+            {batchSize: 50000, parallel: true, concurrency: 8}
         );
     """
-    execute_and_print_query(driver, query)
+    execute_query(driver, query, True)
 
 
 def main(driver: Driver) -> None:
