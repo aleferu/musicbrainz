5e4e158ce13faabd0a2431f563d0496e86cb12a7
aleferu <aleferu888@gmail.com>
2024-10-30 18:58:49 +0100

    feat: Releases to Tracks progression

--- pre_lfm/add_id_final_tracks_csv.py
+++ pre_lfm/add_id_final_tracks_csv.py
@@ -6,7 +6,7 @@ import pandas as pd
 
 if __name__ == '__main__':
     print("Reading...")
-    data = pd.read_csv("releases_no_va_merged.csv", dtype=str)
+    data = pd.read_csv("tracks_no_va_merged.csv", dtype=str)
 
     print("Removing doble quotes...")
     data = data.map(lambda x: x.replace('"', '') if isinstance(x, str) else x)
@@ -15,6 +15,6 @@ if __name__ == '__main__':
     data["id"] = range(len(data))
 
     print("Writing...")
-    data.to_csv("releases_no_va_merged_id.csv", index=False)
+    data.to_csv("tracks_no_va_merged_id.csv", index=False)
 
     print("Done!")
