44b94b7c80c84405bfb307dcdd9b87e92045b975
aleferu <aleferu888@gmail.com>
2025-03-25 08:13:50 +0100

    feat: Some progress regarding MB only data

--- pyg_experiments/build_tags_has_tag_tensor_musicbrainz.py
+++ pyg_experiments/build_tags_has_tag_tensor_musicbrainz.py
@@ -4,6 +4,7 @@
 import torch
 import pickle
 import pandas as pd
+import dask
 from dask import dataframe as dd
 import logging
 import numpy as np
@@ -36,12 +37,36 @@ def build_artists():
     df = df.explode("tags")
     artist_map = get_artist_map()
     tag_map = get_tag_map()
-    df["artist"] = df["artist"].map(artist_map, meta=("artist", str))
-    df["tags"] = df["tags"].map(tag_map, meta=("tags", str))
+    df["artist"] = df["artist"].map_partitions(artist_map, meta=("artist", str))
+    df["tags"] = df["tags"].map_partitions(tag_map, meta=("tags", str))
+    logging.info("Computing...")
     arr = df.to_dask_array().compute().T
-    print(type(arr))
-    print(arr)
-    print(torch.tensor(arr))
+    logging.info("Done! Artist array of shape: %s", str(arr.shape))
+    logging.info("Saving artist tensor...")
+    arr = torch.tensor(arr, dtype=torch.long)
+    torch.save(arr, "pyg_experiments/ds/has_tag_artists_mb.pt")
+    arr[[0, 1], :] = arr[[1, 0], :]
+    torch.save(arr, "pyg_experiments/ds/tags_artists_mb.pt")
+
+
+def build_tracks():
+    df = dd.read_csv("data/tracks_no_va_merged_id_clean.csv", dtype=str, blocksize="128M")
+    df["tags"] = df["tags"].str.split(", ")
+    df = df.explode("tags")
+    df = df.dropna(subset="tags")
+    track_map = get_track_map()
+    tag_map = get_tag_map()
+    df["id"] = df["id"].map_partitions(track_map, meta=("artist", str))
+    df["tags"] = df["tags"].map_partitions(tag_map, meta=("tags", str))
+    df = df[["id", "tags"]]
+    logging.info("Computing...")
+    arr = df.to_dask_array().compute().T
+    logging.info("Done! Track array of shape: %s", str(arr.shape))
+    logging.info("Saving track tensor...")
+    arr = torch.tensor(arr, dtype=torch.long)
+    torch.save(arr, "pyg_experiments/ds/has_tag_tracks_mb.pt")
+    arr[[0, 1], :] = arr[[1, 0], :]
+    torch.save(arr, "pyg_experiments/ds/tags_tracks_mb.pt")
 
 
 if __name__ == '__main__':
@@ -52,4 +77,5 @@ if __name__ == '__main__':
         datefmt="%Y-%m-%d %H:%M:%S",
     )
 
-    build_artists()
+    # build_artists()
+    build_tracks()
